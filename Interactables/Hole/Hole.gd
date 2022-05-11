df4523abb71c5f6e2468a866fa35ac23c0323ebe
Telkan <gamba.gael@orange.fr>
2022-05-11 20:35:20 +0200

    BAM LE CAILLOU DANS LA COQUE

--- Interactables/Hole/Hole.gd
+++ Interactables/Hole/Hole.gd
@@ -17,3 +17,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Area2D_area_entered(area):
+	#TODO Shake caméra & all
+	$Sprite.show()
+	$Sprite/Splinters.set_emitting(true)
+	pass # Replace with function body.
