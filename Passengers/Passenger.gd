0400c713e04a0c2c64c2ff2905fa922121af2da8
Telkan <gamba.gael@orange.fr>
2022-05-12 21:52:49 +0200

    GIVE IT UP FOR THE SECOND NEURON LADIES AND GENTLEMEN

--- Passengers/Passenger.gd
+++ Passengers/Passenger.gd
@@ -6,14 +6,17 @@ var path = []
 var goal = Vector2()
 
 var rng = RandomNumberGenerator.new()
-onready var Nav = get_tree().get_nodes_in_group("Nav")
+onready var Nav = get_tree().get_nodes_in_group("Nav")[0]
 
 export(Array,NodePath) var ListOfRooms
 
+enum State {MOVING, IDLING, WAITING}
+var currentState = State.MOVING
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
-	Nav = Nav[0]
+	chooseRandomNextMove()
+	
 	pass # Replace with function body.
 
 
@@ -29,13 +32,44 @@ func followPath(delta):
 	else:
 		path.remove(0)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func chooseRandomNextMove():
+	var listOfRooms = get_tree().get_nodes_in_group("Rooms")
+	var chosenRoom = rng.randi_range(0,listOfRooms.size()-1)
+	setGoal(listOfRooms[chosenRoom].get_position())
+
+func movingState(delta):
 	if path.size() >= 1:
-		print("on est là?")
 		followPath(delta)
 	else:
-		var listOfRooms = get_tree().get_nodes_in_group("Rooms")
-		var chosenRoom = rng.randi_range(0,listOfRooms.size()-1)
-		setGoal(listOfRooms[chosenRoom].get_position())
-		print("on est là?")
+		currentState = State.IDLING
+
+func idleState(delta):
+	#Decide to move
+	if rng.randf() >0.9:
+		chooseRandomNextMove()
+		currentState = State.MOVING
+	#Small Idle Movement
+	elif rng.randf() > 0.5:
+		var currentPos = get_position()
+		currentPos.x += rng.randi_range(-30,30)
+		setGoal(currentPos)
+		currentState = State.MOVING
+	#Static for a while
+	else:
+		get_tree().create_timer(rng.randf_range(0,2)).connect("timeout", self, "stopWaiting")
+		currentState = State.WAITING
+
+
+func stopWaiting():
+	currentState = State.IDLING
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if currentState == State.MOVING:
+		movingState(delta)
+	elif currentState == State.IDLING:
+		idleState(delta)
+	elif currentState == State.WAITING:
+		pass
+	else:
+		currentState = State.IDLING 
